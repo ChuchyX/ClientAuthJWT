67f980829a8dfd3790af6bfd0834e053d3c4de31
ChuchyX <jesuspenax11@gmail.com>
2022-10-03 13:31:30 -0400

    'first'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './models/User';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appJwt';
+
+  user = new User();
+
+  constructor(private authService: AuthService){
+
+  }
+
+  register(user: User)
+  {
+    this.authService.register(user).subscribe();
+  }
+
+  login(user: User)
+  {
+    this.authService.login(user).subscribe((token: string) => {
+      localStorage.setItem('authToken', token);
+    })
+  }
+
+  getMe()
+  {
+    this.authService.getMe().subscribe((name: string) => {
+      console.log(name);
+    })
+  }
 }
